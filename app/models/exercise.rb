8836b3039247cf2430fe01b596789fe94dba704a
Mario Villarroel <movilla@gmail.com>
2019-08-12 01:21:58 -0300

    Limited listing to last 7 days only.

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -4,4 +4,9 @@ class Exercise < ApplicationRecord
   validates :duration_in_minutes, numericality: {
     only_integer: true, greater_than: 0
   }
+  validates :workout_date, presence: true
+
+  default_scope {
+    where('workout_date > ?', 7.days.ago).order(workout_date: :desc)
+  }
 end
--- spec/features/excersices/listing_exercises_spec.rb
+++ spec/features/excersices/listing_exercises_spec.rb
@@ -5,16 +5,27 @@ RSpec.feature "Listing exercises" do
     @john = User.create(email: "john@here.com", password: "test1234")
     login_as(@john)
 
-    @ex1 = @john.exercises.create()
-    @ex2 = @john.exercises.create()
+    @ex1 = @john.exercises.create(duration_in_minutes: "10", workout: "This is it", workout_date: 3.days.ago)
+    @ex2 = @john.exercises.create(duration_in_minutes: "5", workout: "That is not", workout_date: 2.days.ago)
+    @ex3 = @john.exercises.create(duration_in_minutes: "12", workout: "laying down", workout_date: 9.days.ago)
   end
   scenario "Shows User's workout for the last 7 days" do
+    visit "/"
+    click_link "My Lounge"
+    expect(page).to have_content(@ex1.workout)
+    expect(page).to have_content(@ex1.workout_date)
+    expect(page).to have_content(@ex1.workout)
+    expect(page).to have_content(@ex2.workout_date)
+  end
+
+  scenario "Latest 7 days only show" do
     visit "/"
     click_link "My Lounge"
 
     expect(page).to have_content(@ex1.duration_in_minutes)
     expect(page).to have_content(@ex1.workout)
     expect(page).to have_content(@ex2.workout_date)
+    expect(page).not_to have_content(@ex3.workout)
   end
 
   scenario "No workouts created" do
