83140f068065e11ea07972aa77f5e0ca8ac3b071
Mario Villarroel <movilla@gmail.com>
2019-08-11 23:18:42 -0300

    Exercises created properly working, still needs "beautyness"

---
+++ app/controllers/exercises_controller.rb
@@ -0,0 +1,33 @@
+# Class for exercises
+class ExercisesController < ApplicationController
+  before_action :set_exercise, only: [:show, :destroy, :edit]
+
+  def index; end
+  def edit; end
+  def show; end
+
+  def new
+    @user = current_user
+    @exercise = Exercise.new
+  end
+
+  def create
+    @exercise = Exercise.new(exercise_params.merge(user_id: current_user.id))
+    if @exercise.save
+      redirect_to user_exercise_path(current_user, @exercise), notice: "Exercise has been created"
+    else
+      @user = current_user
+      render :new
+    end
+  end
+
+  private
+
+  def exercise_params
+    params.require(:exercise).permit(:workout, :workout_date, :duration_in_minutes)
+  end
+
+  def set_exercise
+    @exercise = Exercise.find(params[:id])
+  end
+end
